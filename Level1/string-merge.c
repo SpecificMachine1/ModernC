98fbbb342b7d717295d08db632661f257a8cd78c
SpecificMachine1 <fjt72701@yahoo.com>
2025-11-12 21:19:10 -0500

    start working on Challenge 1
    started working on a string merge sort still have several things to
    figure out first

---
+++ Level1/string-merge.c
@@ -0,0 +1,9 @@
+/* Challenge 1 A Merge Sort with Recursion
+ * takes in file(s) and sorts their (case-insensitive words
+ * alphabetically. Need to figure out file IO, strings at least
+ * although to start with I may just take in some words from the command line */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
